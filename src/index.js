1d1ba49d71a4617e92c462873bff46b362633be9
Руслан Макиев <ruslanmakiev@Ruslans-MacBook-Air.local>
2023-02-06 22:48:06 +0300

    Commit changes

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux'
+import { store, persistor } from './store/store'
+
+import { PersistGate } from 'redux-persist/integration/react'
+
+import App from './App';
+
+import './index.scss';
+
+const rootElement = document.getElementById('root');
+
+render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<PersistGate persistor={persistor}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
+	</React.StrictMode>,
+	rootElement
+);
