f395091ba6731a4b0d1b30015e5a7c20808d39ba
Ruslan <105549663+russellsoft@users.noreply.github.com>
2023-02-07 22:43:45 +0300

    Add files via upload

---
+++ src/store/categories/category.action.js
@@ -0,0 +1,16 @@
+import { createAction } from "../../utils/reducer/reducer.utils"
+import { CATEGORIES_ACTION_TYPES } from "./category.types"
+
+export const fetchCategoriesStart = () => {
+	return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+}
+
+export const fetchCategoriesSuccess = (categoriesArray) => {
+	return createAction(
+		CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+		categoriesArray)
+}
+
+export const fetchCategoriesFailed = (error) => {
+	return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
+}
